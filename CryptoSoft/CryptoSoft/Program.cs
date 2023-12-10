5a6c77c4e438505af7efa40feff77094d2a2dbe2
RITTER Antoine <antoine.ritter@viacesi.fr>
2023-12-10 20:31:27 +0100

    feat: cryptosoft working and callable

--- CryptoSoft/CryptoSoft/Program.cs
+++ CryptoSoft/CryptoSoft/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+class CryptoSoft
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: CryptoSoft <text_to_encrypt> <key>");
+            return;
+        }
+
+        string textToEncrypt = args[0];
+        string key = args[1];
+
+        string encryptedText = Encrypt(textToEncrypt, key);
+
+        Console.WriteLine(encryptedText);
+    }
+
+    static string Encrypt(string text, string key)
+    {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            encryptedText.Append((char)(text[i] ^ key[i % key.Length]));
+        }
+
+        return encryptedText.ToString();
+    }
+}
