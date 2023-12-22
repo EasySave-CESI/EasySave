476775bd603637a8da4ceb8feaababaf42e4805d
RITTER Antoine <antoine.ritter@viacesi.fr>
2023-12-22 02:05:23 +0100

    fix: try 9

--- EasySaveWPF/MVVM/Models/ClientModel.cs
+++ EasySaveWPF/MVVM/Models/ClientModel.cs
@@ -1,91 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using EasySaveWPF.MVVM.Models;
 using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
 
-namespace EasySaveWPF.MVVM.Models
+internal class ClientModel
 {
-    internal class ClientModel
+    public List<SaveProfile> saveProfiles = new List<SaveProfile>();
+    private readonly Socket clientSocket;
+
+    public ClientModel()
     {
-        public List<SaveProfile> saveProfiles = new List<SaveProfile>();
-        private readonly Socket clientSocket;
+        clientSocket = SeConnecter();
+        ReceiveProfiles();
+    }
 
-        public ClientModel()
+    private Socket SeConnecter()
+    {
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.1.67"), 46154);
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        try
         {
-            clientSocket = SeConnecter();
+            socket.Connect(ipep);
+            return socket;
         }
-
-
-        private Socket SeConnecter()
+        catch (Exception ex)
         {
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.1.67"), 46154);
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            MessageBox.Show("Impossible de se connecter au serveur");
+            return null;
+        }
+    }
 
+    private void Deconnecter()
+    {
+        try
+        {
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Erreur lors de la déconnexion : " + ex.Message);
+        }
+    }
+
+    public void ReceiveProfiles()
+    {
+        Task.Run(() =>
+        {
             try
             {
-                socket.Connect(ipep);
-                return socket;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Impossible de se connecter au serveur");
-                return null;
-            }
-        }
-
-        private void Deconnecter()
-        {
-            try
-            {
-                if (clientSocket != null && clientSocket.Connected)
+                while (true)
                 {
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erreur lors de la déconnexion : " + ex.Message);
-            }
-        }
+                    byte[] data = new byte[1024];
+                    int size = clientSocket.Receive(data);
+                    string json = Encoding.UTF8.GetString(data, 0, size);
 
-        public void ReceiveProfiles()
-        {
-            Task.Run(() =>
-            {
-                try
-                {
-                    while (true)
+                    // Désérialisez le JSON pour obtenir l'objet SaveProfile
+                    SaveProfile receivedProfile = JsonConvert.DeserializeObject<SaveProfile>(json);
+
+                    // Si un profil avec le même nom existe déjà, remplacez-le
+                    if (saveProfiles.Exists(profile => profile.Name == receivedProfile.Name))
                     {
-                        byte[] data = new byte[1024];
-                        int size = clientSocket.Receive(data);
-                        string json = Encoding.UTF8.GetString(data, 0, size);
-
-                        // Désérialisez le JSON pour obtenir l'objet SaveProfile
-                        SaveProfile receivedProfile = JsonConvert.DeserializeObject<SaveProfile>(json);
-
-                        // Si un profil avec le même nom existe déjà, remplacez-le
-                        if (saveProfiles.Exists(profile => profile.Name == receivedProfile.Name))
-                        {
-                            saveProfiles[saveProfiles.FindIndex(profile => profile.Name == receivedProfile.Name)] = receivedProfile;
-                        }
-                        else
-                        {
-                            saveProfiles.Add(receivedProfile);
-                        }
-
-                        Task.Delay(200).Wait();
+                        saveProfiles[saveProfiles.FindIndex(profile => profile.Name == receivedProfile.Name)] = receivedProfile;
                     }
+                    else
+                    {
+                        saveProfiles.Add(receivedProfile);
+                    }
+
+                    Task.Delay(200).Wait();
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erreur lors de la réception des profils : " + ex.Message);
-                }
-            });
-        }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la réception des profils : " + ex.Message);
+            }
+        });
     }
 }
--- EasySaveWPF/MVVM/Models/ServerModel.cs
+++ EasySaveWPF/MVVM/Models/ServerModel.cs
@@ -1,78 +1,75 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Net;
+﻿using EasySaveWPF.MVVM.Models;
+using Newtonsoft.Json;
 using System.Net.Sockets;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace EasySaveWPF.MVVM.Models
+internal class ServerModel
 {
-    internal class ServerModel
+    private static IPEndPoint clientep;
+    public List<SaveProfile> saveProfiles = new List<SaveProfile>();
+    private static Socket clientSocket;
+
+    public ServerModel()
     {
-        private static IPEndPoint clientep;
-        public List<SaveProfile> saveProfiles = new List<SaveProfile>();
-        private static Socket clientSocket;
+        Socket serverSocket = SeConnecter();
+        clientSocket = AccepterConnexion(serverSocket);
+        StartSendingProfiles();
+    }
 
-        public ServerModel()
+    private static Socket SeConnecter()
+    {
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 46154);
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(ipep);
+        socket.Listen(10);
+        return socket;
+    }
+
+    private static Socket AccepterConnexion(Socket socket)
+    {
+        Socket client = socket.Accept();
+        clientep = (IPEndPoint)client.RemoteEndPoint;
+        return client;
+    }
+
+    private static void Deconnecter(Socket socket)
+    {
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+
+    public void StartSendingProfiles()
+    {
+        Task.Run(async () =>
         {
-            Socket serverSocket = SeConnecter();
-            clientSocket = AccepterConnexion(serverSocket);
-        }
-
-        private static Socket SeConnecter()
-        {
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 46154);
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Bind(ipep);
-            socket.Listen(10);
-            return socket;
-        }
-
-        private static Socket AccepterConnexion(Socket socket)
-        {
-            Socket client = socket.Accept();
-            clientep = (IPEndPoint)client.RemoteEndPoint;
-            return client;
-        }
-
-
-        private static void Deconnecter(Socket socket)
-        {
-            socket.Shutdown(SocketShutdown.Both);
-            socket.Close();
-        }
-
-        public void StartSendingProfiles()
-        {
-            Task.Run(async () =>
+            while (true)
             {
-                while (true)
+                foreach (SaveProfile profile in saveProfiles)
                 {
-                    foreach (SaveProfile profile in saveProfiles)
-                    {
-                        SendProfile(profile);
-                        await Task.Delay(200);
-                    }
+                    SendProfile(profile);
+                    await Task.Delay(200);
                 }
-            });
-        }
+                // Ajoutez une pause entre les envois pour séparer les profils
+                await Task.Delay(2000);
+            }
+        });
+    }
 
-        private static void SendProfile(SaveProfile profile)
+    private static void SendProfile(SaveProfile profile)
+    {
+        try
         {
-            try
-            {
-                // Sérialisez l'objet SaveProfile en JSON
-                string json = JsonConvert.SerializeObject(profile);
-                byte[] data = Encoding.UTF8.GetBytes(json);
+            // Sérialisez l'objet SaveProfile en JSON
+            string json = JsonConvert.SerializeObject(profile);
+            byte[] data = Encoding.UTF8.GetBytes(json);
 
-                // Envoyez les données au client
-                clientSocket.Send(data);
-            }
-            catch (Exception ex)
-            {
-                // Gérez les exceptions liées à l'envoi du profil
-            }
+            // Envoyez les données au client
+            clientSocket.Send(data);
+        }
+        catch (Exception ex)
+        {
+            // Gérez les exceptions liées à l'envoi du profil
         }
     }
 }
